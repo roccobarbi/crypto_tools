7ada1742d34cfc82b4456ec7f3b5549f2082a271
rocco barbini <roccobarbi@gmail.com>
2021-11-25 08:50:28 +0100

    Frequencies are now calculated

---
+++ crypto_tools/stats/letter_frequency.py
@@ -0,0 +1,40 @@
+import functools
+
+from letter_counter import LetterCounter
+
+
+class LetterFrequency:
+    def __init__(self, text=""):
+        self.text = text
+        self.frequency = {}
+        self.letter_count = LetterCounter(text=text)
+
+    def __repr__(self):
+        return 'LetterFrequency(text=%s)' % self.text
+
+    def __str__(self):
+        frequency = self.calculate()
+        return '[%s]' % ({", ".join(": ".join([str(c), str(counter[c])]) for c in counter)})
+
+    def calculate(self):
+        if len(self.text.strip()) == 0:
+            return {}
+        if self.frequency == {}:
+            counter = self.letter_count.count()
+            length = 0
+            for item in counter.keys():
+                length += counter[item]
+            for item in counter.keys():
+                self.frequency[item] = counter[item] / length
+        return self.frequency
+
+    def set_text(self, text):
+        self.text = text
+        self.frequency = {}
+        self.letter_count = LetterCounter(text=text)
+
+    def get_text(self):
+        return self.text
+
+    def get_alphabet(self):
+        return self.letter_count.get_alphabet()
