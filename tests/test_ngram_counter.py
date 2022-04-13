ce217b100754f5e9a57accee638a3b356f8583ad
rocco barbini <roccobarbi@gmail.com>
2022-04-13 23:12:48 +0200

    NgramCounter now manages strings both in their joined form (without whitespace), and in their unjoined form (only ngrams inside words).

--- tests/test_ngram_counter.py
+++ tests/test_ngram_counter.py
@@ -46,6 +46,37 @@ class TestNgramCounter(unittest.TestCase):
         }
         self.assertEqual(counter, correct)
 
+    def test_count_text_not_joined(self):
+        text = "The quick brown fox jumps over the lazy dog"
+        counter = ngram_counter.NgramCounter(nvalue=2, text=text, joined=False).count()
+        correct = {
+            "th": 2,
+            "he": 2,
+            "qu": 1,
+            "ui": 1,
+            "ic": 1,
+            "ck": 1,
+            "br": 1,
+            "ro": 1,
+            "ow": 1,
+            "wn": 1,
+            "fo": 1,
+            "ox": 1,
+            "ju": 1,
+            "um": 1,
+            "mp": 1,
+            "ps": 1,
+            "ov": 1,
+            "ve": 1,
+            "er": 1,
+            "la": 1,
+            "az": 1,
+            "zy": 1,
+            "do": 1,
+            "og": 1,
+        }
+        self.assertEqual(counter, correct)
+
 
 if __name__ == '__main__':
     unittest.main()
