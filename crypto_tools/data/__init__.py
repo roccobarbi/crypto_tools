9adce1fa8efa7a46ee8e8988b50174bf712999ce
rocco barbini <roccobarbi@gmail.com>
2022-04-26 22:33:35 +0200

    frequent_short_words_by_language added

--- crypto_tools/data/__init__.py
+++ crypto_tools/data/__init__.py
@@ -1,2 +1,3 @@
 from .index_of_coincidence_by_language import index_of_coincidence_by_language
 from .letter_frequency_by_language import letter_frequency_by_language
+from .frequent_short_words_by_language import frequent_short_words_by_language
---
+++ crypto_tools/data/frequent_short_words_by_language.py
@@ -0,0 +1,7 @@
+frequent_short_words_by_language = {
+    "en": {
+        1: ["a", "i"],
+        2: ["we", "am", "it", "as", "if", "an", "at"],
+        3: ["the", "and", "can", "are"]
+    }
+}
