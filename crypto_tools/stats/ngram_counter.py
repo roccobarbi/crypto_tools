35a055341380e35f3924bfe11e89976bd9a17557
rocco barbini <roccobarbi@gmail.com>
2022-05-10 00:05:46 +0200

    Avoid non-alphabetic characters by default (overridable) on NgramCounter.

--- crypto_tools/stats/ngram_counter.py
+++ crypto_tools/stats/ngram_counter.py
@@ -2,17 +2,24 @@ from ..utils import string_utils
 
 
 class NgramCounter:
-    def __init__(self, nvalue=2, text="", joined=True):
-        self.text = text
+    def __init__(self, nvalue=2, text="", joined=True, alphabet="abcdefghijklmnopqrstuvwxyz", lower=True):
+        if lower:
+            self.text = text.lower()
+        else:
+            self.text = text
         self.nvalue = int(nvalue)
         self.joined = joined
         self.counter = {}
+        self.alphabet = alphabet
+        self.lower = lower
 
     def __repr__(self):
-        return 'NgramCounter(nvalue={nvalue}, text={text}, joined={joined})'.format(
+        return 'NgramCounter(nvalue={nvalue}, text={text}, joined={joined}, alphabet={alphabet}, lower={lower})'.format(
             nvalue=self.nvalue,
             text=self.text,
-            joined=self.joined
+            joined=self.joined,
+            alphabet=self.alphabet,
+            lower=self.lower
         )
 
     def __str__(self):
@@ -26,11 +33,14 @@ class NgramCounter:
             else:
                 text = self.text
             for position, character in enumerate(text[:-self.nvalue + 1]):
+                if self.counter == {}:
+                    print(text[position:position + self.nvalue])
                 if not string_utils.has_whitespace(text[position:position + self.nvalue]):
-                    if text[position:position + self.nvalue].lower() not in self.counter.keys():
-                        self.counter[text[position:position + self.nvalue].lower()] = 1
-                    else:
-                        self.counter[text[position:position + self.nvalue].lower()] += 1
+                    if not string_utils.includes_other_letters(text[position:position + self.nvalue], self.alphabet):
+                        if text[position:position + self.nvalue].lower() not in self.counter.keys():
+                            self.counter[text[position:position + self.nvalue].lower()] = 1
+                        else:
+                            self.counter[text[position:position + self.nvalue].lower()] += 1
         return self.counter
 
     def set_text(self, text):
