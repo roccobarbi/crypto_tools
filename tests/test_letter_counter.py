dbc0a87c99ac729f9ffa0c949a4affeeb7310858
rocco barbini <roccobarbi@gmail.com>
2022-05-10 22:52:25 +0200

    Test letter_counter with non alphabetic characters

--- tests/test_letter_counter.py
+++ tests/test_letter_counter.py
@@ -40,6 +40,39 @@ class TestLetterCounter(unittest.TestCase):
         }
         self.assertEqual(counter, correct)
 
+    def test_count_text_with_nonalphabetic_characters(self):
+        text = "The quick, brown fox jumps over the lazy dog!"
+        counter = letter_counter.LetterCounter(text).count()
+        correct = {
+            "a": 1,
+            "b": 1,
+            "c": 1,
+            "d": 1,
+            "e": 3,
+            "f": 1,
+            "g": 1,
+            "h": 2,
+            "i": 1,
+            "j": 1,
+            "k": 1,
+            "l": 1,
+            "m": 1,
+            "n": 1,
+            "o": 4,
+            "p": 1,
+            "q": 1,
+            "r": 2,
+            "s": 1,
+            "t": 2,
+            "u": 2,
+            "v": 1,
+            "w": 1,
+            "x": 1,
+            "y": 1,
+            "z": 1,
+        }
+        self.assertEqual(counter, correct)
+
 
 if __name__ == '__main__':
     unittest.main()
