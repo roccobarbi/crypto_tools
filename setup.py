352b00061f372618ffd3f903588f1519593a0b2b
rocco barbini <roccobarbi@gmail.com>
2022-05-11 23:04:00 +0200

    1.0.10

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="crypto_tools",
     packages=find_packages(exclude=['tests*']),
-    version="1.0.9",
+    version="1.0.10",
     author="Rocco Barbini",
     author_email="roccobarbi@gmail.com",
     description="Tools that can be used to build software for breaking \"traditional\" cyphers and codes.",
     long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
     long_description=long_description,
     url="https://github.com/roccobarbi/crypto_tools",
-    download_url="https://github.com/roccobarbi/crypto_tools/releases/tag/1.0.9",
+    download_url="https://github.com/roccobarbi/crypto_tools/releases/tag/1.0.10",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
